7ce570b37ceb721f0a471e1a55e11932ed28282c
Venkatakalyani123 <kalyanijada646@gmail.com>
2025-03-24 12:31:13 +0530

    Update DbConnection.java

--- bus-reservation-main/bus-reservation-main/DbConnection.java
+++ bus-reservation-main/bus-reservation-main/DbConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class DbConnection {
 	public static Connection getConnection() throws SQLException{
-		return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hr","sathya");
+		return DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","hr","Kalyani@123");
 		
 	}
 
